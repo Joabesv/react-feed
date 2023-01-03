f9750b1e116fbfdb72800697b274f1f6b5be8aa5
Joabesv <joabevarjao123@gmail.com>
2023-01-03 14:14:20 -0300

    feat(post-html): styling the posts

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -6,24 +6,62 @@ export function Post() {
       <article className={styles.post}>
         <header>
           <div className={styles.author}>
-            <img src="https://github.com/Joabesv.png" className={styles.avatar} alt="" />
+            <img
+              src="https://github.com/Joabesv.png"
+              className={styles.avatar}
+              alt=""
+            />
             <div className={styles.authorInfo}>
               <strong>Joabe Silva</strong>
               <span>Dev</span>
             </div>
           </div>
 
-          <time title="Publicado faz cota" dateTime="2022-05-13">Publicado há 1h</time>
+          <time title="Publicado faz cota" dateTime="2022-05-13">
+            Publicado há 1h
+          </time>
         </header>
 
         <div className={styles.content}>
+          <p>Need a job, share my linkedin if you can, would help a lot :)</p>
           <p>
-            Need a job, share my linkedin if you can, would help a lot :)
+            👉{' '}<a href="https://www.linkedin.com/in/Joabesv">Joabe Silva</a>
           </p>
           <p>
-            <a href="https://www.linkedin.com/in/Joabesv"></a>
+            <a href="">#jobs #javascript #dev</a>
           </p>
-          <p><a href="">#jobs #javascript #dev</a></p>
+        </div>
+      </article>
+
+      <article className={styles.post}>
+        <header>
+          <div className={styles.author}>
+            <img
+              src="https://github.com/Joabesv.png"
+              className={styles.avatar}
+              alt=""
+            />
+            <div className={styles.authorInfo}>
+              <strong>Joabe Silva</strong>
+              <span>Dev</span>
+            </div>
+          </div>
+
+          <time title="Publicado faz cota" dateTime="2022-05-13">
+            Publicado há 1h
+          </time>
+        </header>
+
+        <div className={styles.content}>
+          <p>Need a job, share my linkedin if you can, would help a lot :)</p>
+          <p>
+            👉{' '}<a href="https://www.linkedin.com/in/Joabesv">Joabe Silva</a>
+          </p>
+          <p>
+            <a href="">#jobs</a>{''}
+            <a href="">#javascript</a>{''}
+            <a href="">#dev</a>{''}
+          </p>
         </div>
       </article>
     </>

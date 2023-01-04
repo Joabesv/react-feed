10a12398f6f667cfaf518646c6b784c57a458cee
Joabesv <joabevarjao123@gmail.com>
2023-01-03 21:59:09 -0300

    feat(comment): creating the comment structure

---
+++ src/components/Comment.tsx
@@ -0,0 +1,34 @@
+import { Trash, ThumbsUp } from 'phosphor-react'
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/Joabesv.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Joabe Silva</strong>
+              <time title="Publicado faz cota" dateTime="2022-05-13">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom, parabéns!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
